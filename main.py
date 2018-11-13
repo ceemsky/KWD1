0429cb25b878b4b4be882cc890c202b698d7f84f
Michal Czernikowski <38218975+ceemsky@users.noreply.github.com>
2018-11-13 23:13:33 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import foos
 
 
 dataLabels= foos.getDataLabels('/home/ceem/PycharmProjects/kwd1/data/iris.data.learning')
-kNN = kNN(3, dataLabels)
-data= []
-data.append(kNN.dataLabels[14])
-data.append(kNN.dataLabels[31])
-temp=kNN.predict(foos.getData(data))
-print(kNN.score(data, temp))
+testData = foos.getDataLabels('/home/ceem/PycharmProjects/kwd1/data/iris.data.test')
+data=foos.getData(testData)
+labels=foos.getLabels(testData)
+kNN = kNN(15, dataLabels)
+
+
+temp=kNN.predict(data)
+print(kNN.score(testData, temp))
